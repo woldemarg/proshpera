3e896bab7a7a723ffb6d1b321369d3980e601ae3
Volodymyr Holomb <wldmrgml@gmail.com>
2023-09-21 14:19:06 +0300

    update

--- demo/prosphera_demo.py
+++ demo/prosphera_demo.py
@@ -1,5 +1,12 @@
+import pandas as pd
+from sklearn import datasets
 from sklearn.datasets import make_blobs
 from prosphera.projector import Projector
+
+# %%
+
+# Instantiate the class
+visualizer = Projector()
 
 # %%
 
@@ -10,10 +17,45 @@ data, labels = make_blobs(
     n_features=25,
     random_state=1234)
 
+# Call the visualize method to generate and display the visualization
+visualizer.project(
+    data=data,
+    labels=labels)
+
 # %%
 
-# Instantiate the class
-visualizer = Projector()
+wine = datasets.load_wine()
 
-# Call the visualize method to generate and display the visualization
-visualizer.project(data=data, labels=labels)
+visualizer.project(
+    data=wine['data'],
+    labels=wine['target'])
+
+# %%
+
+cancer = datasets.load_breast_cancer()
+
+visualizer.project(
+    data=cancer['data'],
+    labels=cancer['target'])
+
+# %%
+
+digits = datasets.load_digits(n_class=5)
+
+visualizer.project(
+    data=digits['data'],
+    meta=digits['target'])
+
+# %%
+
+visualizer.project(
+    data=digits['data'],
+    labels=digits['target'])
+
+# %%
+
+housing = datasets.fetch_california_housing()
+
+visualizer.project(
+    data=housing['data'],
+    labels=pd.qcut(housing['data'][:, 1], 5).astype(str))
